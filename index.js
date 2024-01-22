a92ffa9225ffa4255d3af24019ac23533752535b
SerDunk <abhinavkondapalli7@gmail.com>
2024-01-22 19:46:22 +0530

    Added functionality to 'New' Button for form pop up

---
+++ index.js
@@ -0,0 +1,8 @@
+const addBtn=document.querySelector('.add')
+const bookForm=document.querySelector('.book-add')
+
+
+addBtn.addEventListener('click',()=>{
+    bookForm.classList.remove('invisible')
+    
+})
