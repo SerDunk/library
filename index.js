67badf09e4e11d0aa4054c30b9e1877b56820cce
SerDunk <abhinavkondapalli7@gmail.com>
2024-01-28 14:22:49 +0530

    Succesfull add and deletion of books from library

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const addBtn=document.querySelector('.add')
 const bookForm=document.querySelector('.book-add')
 const submitBtn=document.querySelector('#submit')
 const pageContent=document.querySelector('.content')
+let indexToRemove
+let dataSet=0
 let bookName
 let bookAuthor
 
@@ -14,22 +16,40 @@ function Book(name,author){
 function addBook(){
     bookName=document.querySelector('#bookName').value
     bookAuthor=document.querySelector('#bookAuthor').value
-    let book=new Book(bookName,bookAuthor)
-    
-
+    book=new Book(bookName,bookAuthor)
     library.push(book)
 }
 
 function createCard(book){
     let card=document.createElement('div')
+    card.setAttribute('data-id',dataSet)
     let title=document.createElement('h1')
     let author=document.createElement('h2')
+    let removeBtn=document.createElement('button')
+
+    removeBtn.setAttribute('data-id',dataSet)
+    removeBtn.setAttribute('class','remove')
+    dataSet=dataSet+1
+    removeBtn.textContent='Remove'
     title.textContent=book.name
     author.textContent='By '+book.author
     card.append(title,author)
+    card.append(removeBtn)
     card.classList.add('card')
     pageContent.appendChild(card)
+    
+    function removeCard(){
+        indexToRemove=removeBtn.getAttribute('data-id')
+        library.splice(indexToRemove,1)
+    }
+    removeBtn.addEventListener('click',()=>{
+        removeCard()
+        let cardToRemove=document.querySelector(`[data-id="${indexToRemove}"]`);
+        cardToRemove.remove()
+        console.log(library)
+    })
 }
+
 
 addBtn.addEventListener('click',()=>{
     bookForm.classList.remove('invisible')
@@ -42,3 +62,6 @@ submitBtn.addEventListener('click',()=>{
     bookForm.classList.add('invisible')
     console.log(library)
 })
+
+
+
